e866c732771d1f67397e4a9833770e1af24e8e5f
Bruno <bruno_silth@hotmail.com>
2021-07-30 10:13:49 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin {
+    public idUsuario: number
+    public nome: string
+    public senha: string
+    public token: string
+    public foto: string
+    public tipo: string
+    public email: string
+
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+
+  ) { }
+
+entrar(userlogin: UserLogin ): Observable<UserLogin>{
+  return this.http.post<UserLogin>('http://localhost:8080/api/v1/usuario/credenciais', userlogin)
+
+}
+
+cadastrar(user: User): Observable<User>{
+  return this.http.post<User>('http://localhost:8080/api/v1/usuario/salvar', user)
+
+}
+
+
+}
